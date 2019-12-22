8c068fcdcf22814565db465829d792c6a1c91f94
Maciej Kotowski <makoteq@gmail.com>
2019-12-22 14:43:04 +0100

    optimize code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Project by Maciej Kotowski https://github.com/makoteq*/
 
-const { app,Menu, BrowserWindow } = require("electron");
+const { app, Menu, BrowserWindow } = require("electron");
 const home = "desktop";
 const fs = require("fs");
 const { ipcMain } = require("electron");
@@ -17,7 +17,6 @@ let isgithub = "none";
 let githublink = "https://github.com/";
 
 ipcMain.on("button", (event, arg) => {
-
   /* fs.writeFile(app.getPath(home)+'/test.txt', 'Hello content!', function (err) {
     if (err) throw err;
    event.reply('info','Saved! in '+app.getPath(home) )
@@ -25,42 +24,39 @@ ipcMain.on("button", (event, arg) => {
   let input = new BrowserWindow({
     width: 600,
     height: 800,
-    icon: __dirname+'./assets/logo.png',
+    icon: __dirname + "./assets/logo.png",
     webPreferences: {
       nodeIntegration: true
     }
-    
-  })
-  input.setMenuBarVisibility(false);
+  });
+ // input.setMenuBarVisibility(false);
   input.loadFile("input.html");
 });
-  ipcMain.on("name", (event, arg) => {
-    name=arg;
-  });
-  ipcMain.on("description", (event, arg) => {
-    description=arg;
-  });
-  ipcMain.on("fbValue", (event, arg) => {
-    facebooklink = arg;
-    isfacebook = "";
-  });
-  ipcMain.on("igValue", (event, arg) => {
-    instagramlink = arg;
-    isinstagram = "";
-  });
-  ipcMain.on("gitValue", (event, arg) => {
-    githublink = arg;
-    isgithub = "";
-  });
+ipcMain.on("name", (event, arg) => {
+  name = arg;
+});
+ipcMain.on("description", (event, arg) => {
+  description = arg;
+});
+ipcMain.on("fbValue", (event, arg) => {
+  facebooklink = arg;
+  isfacebook = "";
+});
+ipcMain.on("igValue", (event, arg) => {
+  instagramlink = arg;
+  isinstagram = "";
+});
+ipcMain.on("gitValue", (event, arg) => {
+  githublink = arg;
+  isgithub = "";
+});
 
-  ipcMain.on("submit", (event, arg) => {
-  
-
-    generate();
-  });
-
+ipcMain.on("submit", (event, arg) => {
+  generate();
+});
 
 function generate() {
+  console.log('generate')
   mkdirp(app.getPath(home) + "/" + name + " portfolio", function(err) {
     if (err) console.error(err);
     fs.writeFile(
@@ -77,23 +73,29 @@ function generate() {
       ),
       () => {}
     );
-    fs.copyFile( __dirname+"/assets/Desktop.png",app.getPath(home) + "/" + name + " portfolio/Desktop.png", err => {
+    fs.copyFile(
+      __dirname + "/assets/Desktop.png",
+      app.getPath(home) + "/" + name + " portfolio/Desktop.png",
+      err => {
         if (err) throw err;
       }
     );
     let info = new BrowserWindow({
       width: 600,
       height: 400,
-      icon: __dirname+'./assets/logo.png',
+      icon: __dirname + "./assets/logo.png",
       webPreferences: {
         nodeIntegration: true
       }
-      
-    })
+    });
+    info.setMenuBarVisibility(false);
     info.loadFile("info.html");
     ipcMain.on("path", (event, arg) => {
-      event.reply('path',app.getPath(home));
-    })
+      event.reply("path", app.getPath(home));
+    });
+    info.on("closed", () => {
+      app.quit();
+    });
     // and load the index.html of the app.
     console.log("Saved! in " + app.getPath(home));
   });
@@ -103,12 +105,12 @@ function createWindow() {
   let win = new BrowserWindow({
     width: 600,
     height: 800,
-    icon: __dirname+'./assets/logo.png',
+    icon: __dirname + "./assets/logo.png",
     webPreferences: {
       nodeIntegration: true
     }
   });
-  win.setMenuBarVisibility(false)
+  win.setMenuBarVisibility(false);
   // and load the index.html of the app.
   win.loadFile("index.html");
   win.on("closed", () => {
@@ -116,6 +118,6 @@ function createWindow() {
   });
 }
 
-app.on("ready", function(){
-  createWindow()
-})
+app.on("ready", function() {
+  createWindow();
+});
